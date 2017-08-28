4dde0d8206c09661791bd15f49423fab4ed306c2
Avequin <avequin.da@gmail.com>
2017-08-28 15:17:45 +0200

    hihi

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var url = require('url');
 var session = require('express-session');
-
-var users = require('./routes/users');
 
 var app = express();
 
@@ -17,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -70,12 +68,9 @@ app.post('/admin', (req, res) => {
         res.render('admin', { title: "BDE | Admin", message: message})
     }
     else {
-        res.render('login', { title: "BDE | Admin" })
+        res.render('login', { title: "BDE | Admin", message: "Mot de passe incorret !" })
     }
 });
-
-
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
